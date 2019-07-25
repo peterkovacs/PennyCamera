c672818dddee8285c0ec2738a48a33a2380917fa
Peter Kovacs <peter@sanebox.com>
2019-07-25 11:22:25 -0400

    Initial Commit

---
+++ PennyCamera/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PennyCamera
+//
+//  Created by Peter Kovacs on 7/25/19.
+//  Copyright © 2019 Kovapps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
